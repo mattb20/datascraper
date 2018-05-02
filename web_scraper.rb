9bf15ac1de5c9e964a860cd1b82597c778d1c9d8
Matthew <matt_burgoyne@live.co.uk>
2018-05-02 13:32:46 +0100

    data scraper gets headline text for pet listing and outputs them to a csv file

--- web_scraper.rb
+++ web_scraper.rb
@@ -4,4 +4,7 @@ require 'Pry'
 require 'csv'
 
 page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0');
-Pry.start(binding);
+parse_page = Nokogiri::HTML(page).css('.content').css('.rows').css('.hdrlnk');
+
+pets_array = parse_page.map{|advert| advert.text};
+CSV.open("pets.csv", "a+"){|csv| csv << pets_array}
